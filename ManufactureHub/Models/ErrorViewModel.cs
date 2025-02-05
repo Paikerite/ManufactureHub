85f2e8696cfb993b359ec4201395d68b68f11a26
Moro_Gajima <norkov900@gmail.com>
2025-02-05 16:49:25 +0200

    first commit

---
+++ ManufactureHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManufactureHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
